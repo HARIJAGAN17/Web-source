4872242c4c2634f21b19ab758830dc0d6defdffe
FantasWolf <jaganhari2002@gmail.com>
2024-03-11 14:09:52 +0530

    function parameters

--- 11.5 js test/test.js
+++ 11.5 js test/test.js
@@ -1,18 +1,28 @@
 //data types
 //variables
 
-var name = "hari";
-console.log(name);
+// var name = "hari";
+// console.log(name);
 
-//promt keyword;
-// var myname = promt("What is your name?");
-//alert("myname :"+myname);
+// //promt keyword;
+// // var myname = promt("What is your name?");
+// //alert("myname :"+myname);
 
-//naming conventions rule;
+// //naming conventions rule;
 
-//length
-console.log(name.length);
+// //length
+// console.log(name.length);
 
-//slice is like substring
+// //slice is like substring
+// console.log(name.slice(0,2));
 
-console.log(name.slice(0,2));
+//casing string
+
+// function
+// function functionname(){};
+
+// parameter::
+// function getMIlk(bootles){
+//     var cost = bottles*2;
+//     return cost;
+// }
