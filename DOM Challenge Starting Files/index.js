5a0cce916cfe2878a1d9e6d7128489ccdebd7fcd
FantasWolf <jaganhari2002@gmail.com>
2024-03-12 16:45:33 +0530

    changing using fucntions and class list

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,3 +1,8 @@
 document.querySelector("ul").lastElementChild.innerHTML = "hello";
 
-document.querySelector("li a").style.color="red";
+document.querySelector("li a").style.color="red"; 
+
+function change()
+{
+    document.getElementById("btn").classList.add("hidden");
+}
