d69ed08cb2c58fba9461e10f6992c5c36397dfc1
FantasWolf <jaganhari2002@gmail.com>
2024-03-13 15:24:30 +0530

    using on event

--- 11.9 jQuery/index.js
+++ 11.9 jQuery/index.js
@@ -3,3 +3,18 @@ $("button").css("color","red");
 $("button").click(function(){
     $("button").css("background","pink");
 });
+
+//challenge
+
+$(document).keydown(function(event){
+    $("h1").text(event.key);
+})
+
+//on
+
+// $("h1").on("mouseover",function(){
+//     $("h1").css("background","red");
+// })
+// $("h1").on("mouseout",function(){
+//     $("h1").css("background","orange");
+// })
