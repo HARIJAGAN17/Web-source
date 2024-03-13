e1d3697b01d0af514a4a9344fd79eb9d866b647e
FantasWolf <jaganhari2002@gmail.com>
2024-03-13 19:16:06 +0530

    step4

--- 12.1 Simon Game Challenge Starting Files/game.js
+++ 12.1 Simon Game Challenge Starting Files/game.js
@@ -2,6 +2,9 @@
 var buttonColours = ["red","blue","green","yellow"];
 
 var gamePattern = [];
+
+var userClickedPattern = [];
+
 function nextSequence()
 {
     var randomNumber = Math.floor(Math.random()*4);
@@ -11,5 +14,11 @@ function nextSequence()
     var audio = new Audio("sounds/"+randomChosenColour+".mp3");
     audio.play();
 }
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+});
 
 //nextSequence();
