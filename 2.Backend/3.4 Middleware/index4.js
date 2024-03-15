cfddd7318be78cc8fdda90941312c7779959637b
FantasWolf <jaganhari2002@gmail.com>
2024-03-15 16:16:41 +0530

    updated middleware

--- 2.Backend/3.4 Middleware/index4.js
+++ 2.Backend/3.4 Middleware/index4.js
@@ -1,8 +1,28 @@
 import express from "express";
+import { dirname }  from "path";
+import { fileURLToPath } from "url";
+import  bodyparser  from  "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+
+app.use(bodyparser.urlencoded({
+extended:true,
+}));
+
+app.get("/",(req,res)=>{
+   res.sendFile(__dirname+"/public/index.html")
+  });
+
+  app.post("/submit",(req,res)=>{
+    res.send(`<h1>Your band name is ${req.body["street"]+req.body["pet"]}</h1>`);
+ });
+
+app.listen(port,()=>{
+ console.log(`Listening to the port ${port}`);
 });
+
+
