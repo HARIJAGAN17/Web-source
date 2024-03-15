be0c0fdcf7e9c78c4c878d84e717562dca15e2e1
FantasWolf <jaganhari2002@gmail.com>
2024-03-15 11:28:32 +0530

    Get req res

--- 2.Backend/3.1 Express Server/index.js
+++ 2.Backend/3.1 Express Server/index.js
@@ -2,6 +2,22 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+
+//getting req,res
+app.get("/",(req,res)=>{
+   res.send("<h1>Hello started</h1>");
+});
+
+app.get("/about",(req,res)=>{
+  res.send("<h1>About page</h1>")
+});
+app.get("/contact",(req,res)=>{
+  res.send("<h1>Contact page</h1>")
+});
+
+
+
+//starting server
 app.listen(port,()=>{
   console.log(`Server running on port ${port}.`);
 });
