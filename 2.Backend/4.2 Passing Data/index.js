8fec19c4dbf04bb13c3d55d19bcde1e2c4eca9f5
FantasWolf <jaganhari2002@gmail.com>
2024-03-16 10:34:28 +0530

    UPDATED ejs passing data

--- 2.Backend/4.2 Passing Data/index.js
+++ 2.Backend/4.2 Passing Data/index.js
@@ -1,15 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { render } from "ejs";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const number = req.body["fName"].length+req.body["lName"].length;
+  res.render("index.ejs",{wordLength:number});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
