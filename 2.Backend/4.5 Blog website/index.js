cae6e48fda08f0605b80838fea86432081546369
FantasWolf <jaganhari2002@gmail.com>
2024-03-17 22:25:29 +0530

    "updated the basic styling and data request"

--- 2.Backend/4.5 Blog website/index.js
+++ 2.Backend/4.5 Blog website/index.js
@@ -1,18 +1,24 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 //making css linking
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded(
+  {extended:true}
+))
 //rendering main page
 app.get("/",(req,res)=>{
   res.render("index.ejs");
 });
 
 
+app.post("/", (req, res) => {
+  console.log(req.body); // Logging the form data to the console
+  res.redirect("/"); // Sending a response back to the client
+});
 
 app.listen(port,()=>{
  console.log("Listening to the port:"+port);
