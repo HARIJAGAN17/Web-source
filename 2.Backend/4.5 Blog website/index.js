2bd9e4ea751787188588d8425c1ffae91a5d84d4
FantasWolf <jaganhari2002@gmail.com>
2024-03-17 13:57:32 +0530

    updated blog

--- 2.Backend/4.5 Blog website/index.js
+++ 2.Backend/4.5 Blog website/index.js
@@ -4,11 +4,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+//making css linking
+app.use(express.static("public"));
 
-
+//rendering main page
 app.get("/",(req,res)=>{
   res.render("index.ejs");
 });
+
+
 
 app.listen(port,()=>{
  console.log("Listening to the port:"+port);
