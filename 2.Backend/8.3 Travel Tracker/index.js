3bb85633c4c266ccb5510dc702146630ec8d0d37
FantasWolf <jaganhari2002@gmail.com>
2024-03-28 13:39:15 +0530

    completed tracker

--- 2.Backend/8.3 Travel Tracker/index.js
+++ 2.Backend/8.3 Travel Tracker/index.js
@@ -19,27 +19,44 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
+
+async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visted");
-  let countries =[];
+  let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
+  return countries;
+}
 
-  res.render("index.ejs",{countries:countries, total: countries.length})
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const result = await checkVisisted();
+  res.render("index.ejs",{countries:result, total: result.length})
 });
 
-app.post("/add",async (req,res)=>{
-
+app.post("/add", async (req, res) => {
   const country_name = req.body.country;
-  const result = await db.query("SELECT country_code from countries where country_name = $1",[country_name]);
-
-  if(result.rows.length!=0){
-    await db.query("INSERT INTO visted (country_code) VALUES($1)",[result.rows[0].country_code]);
+  try {
+    const result = await db.query("SELECT country_code from countries where country_name = $1", [country_name]);
+    if (result.rows.length != 0) {
+      try {
+        await db.query("INSERT INTO visted (country_code) VALUES($1)", [result.rows[0].country_code]);
+        res.redirect("/");
+      } catch (error) {
+        const countries = await checkVisisted();
+        res.render("index.ejs", { countries: countries, total: countries.length, error: "Already visited the country" });
+      }
+    } else {
+      throw new Error("Country not exist please try again");
+    }
+  } catch (error) {
+    const countries = await checkVisisted();
+    res.render("index.ejs", { countries: countries, total: countries.length, error: error.message });
   }
-  res.redirect("/")
 });
+
+  
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
