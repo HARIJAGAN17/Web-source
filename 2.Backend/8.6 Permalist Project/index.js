42bb99f6ddec1f345af577d0d08c0691a446f457
FantasWolf <jaganhari2002@gmail.com>
2024-04-01 19:13:34 +0530

    DB retrival done

--- 2.Backend/8.6 Permalist Project/index.js
+++ 2.Backend/8.6 Permalist Project/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from  "pg";
 
 const app = express();
 const port = 3000;
@@ -7,17 +8,50 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+const db = new pg.Client(
+  {
+    user:"postgres",
+    host:"localhost",
+    password:"Iam22@33",
+    database:"permalist",
+    port:5432,
+  }
+);
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+db.connect();
+
+
+
+// let items = [
+//   { id: 1, title: "Buy milk" },
+//   { id: 2, title: "Finish homework" },
+// ];
+
+async function getTitle(){
+  let items = [];
+   const result = await db.query("SELECT id,title FROM items");
+   result.rows.forEach((item)=>{
+
+      const c1 = {
+        id: item.id,
+        title : item.title,
+      }
+      items.push(c1);
+   })
+   return items;
+};
+
+
+app.get("/", async (req, res) => {
+
+const content = await getTitle();
+console.log(content);
+res.render("index.ejs", {
+  listTitle: "Today",
+  listItems: content,
 });
+});
+
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
