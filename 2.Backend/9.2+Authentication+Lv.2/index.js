9fd8c97971f0b37a5dc0aa272ceab6419df51422
FantasWolf <jaganhari2002@gmail.com>
2024-04-02 16:06:38 +0530

    Bcrypt auth 2

--- 2.Backend/9.2+Authentication+Lv.2/index.js
+++ 2.Backend/9.2+Authentication+Lv.2/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 const app = express();
 const port = 3000;
@@ -9,7 +12,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "secrets",
-  password: "123456",
+  password: "Iam22@33",
   port: 5432,
 });
 db.connect();
@@ -41,12 +44,21 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password]
-      );
-      console.log(result);
-      res.render("secrets.ejs");
+
+      bcrypt.hash(password,saltRounds, async (err,hash)=>{
+        if(err){
+          console.log(err);
+        }
+        else{
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            [email, hash]
+          );
+          console.log(result);
+          res.render("secrets.ejs");
+        }
+      })
+      
     }
   } catch (err) {
     console.log(err);
@@ -64,12 +76,21 @@ app.post("/login", async (req, res) => {
     if (result.rows.length > 0) {
       const user = result.rows[0];
       const storedPassword = user.password;
-
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
+      
+      bcrypt.compare(password,storedPassword,(err,result)=>{
+        if(err)
+        {
+          console.log(err);
+        }
+        else{
+          if(result){
+            res.render("secrets.ejs");
+          }
+          else {
+            res.send("Incorrect Password");
+          }
+        }
+      })
     } else {
       res.send("User not found");
     }
