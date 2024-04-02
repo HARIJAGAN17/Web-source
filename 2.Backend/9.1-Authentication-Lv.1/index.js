6b845740c758b395def08d2b248b9630dc234610
FantasWolf <jaganhari2002@gmail.com>
2024-04-02 09:43:47 +0530

    pg init

--- 2.Backend/9.1-Authentication-Lv.1/index.js
+++ 2.Backend/9.1-Authentication-Lv.1/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user:"postgres",
+  database:"secrets",
+  password:"Iam22@33",
+  host:"localhost",
+  port:5432,
+})
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +30,19 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
 
-app.post("/login", async (req, res) => {});
+  const email  = req.body.username;
+  const password  = req.body.password;
+
+});
+
+app.post("/login", async (req, res) => {
+
+  const email  = req.body.username;
+  const password  = req.body.password;
+
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
