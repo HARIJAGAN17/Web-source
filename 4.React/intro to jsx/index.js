96d925061d02b8b41b22426de200e82d4fc61e8b
FantasWolf <jaganhari2002@gmail.com>
2024-04-05 20:22:31 +0530

    intro to jsx

---
+++ 4.React/intro to jsx/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import RreactDom from "react-dom";
+
+ReactDom.render(<h1>Hello mame!</h1>, document.getElementById("root"));
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello mame!";
+// document.getElementById("root").appendChild(h1);
