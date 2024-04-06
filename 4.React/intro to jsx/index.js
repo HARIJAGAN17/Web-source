e9369a0ba44959a9e66be3ae059b19856364a3ae
FantasWolf <jaganhari2002@gmail.com>
2024-04-06 19:30:09 +0530

    update+

--- 4.React/intro to jsx/index.js
+++ 4.React/intro to jsx/index.js
@@ -6,3 +6,5 @@ ReactDom.render(<h1>Hello mame!</h1>, document.getElementById("root"));
 // var h1 = document.createElement("h1");
 // h1.innerHTML = "Hello mame!";
 // document.getElementById("root").appendChild(h1);
+
+//checking ract docs
