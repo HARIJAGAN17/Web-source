a23f5dea6a98d14309f6db188950d75b169a7160
FantasWolf <jaganhari2002@gmail.com>
2024-04-11 10:09:57 +0530

    Added jsx components

---
+++ 4.React/jsx component/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import List from "./List";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <List/>
+  </div>,
+  document.getElementById("root")
+);
+
+//putting these all together in APP and import APP
