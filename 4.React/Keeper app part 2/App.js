69b8a0db203698c429bb94b4ddbc6191262e7cfb
FantasWolf <jaganhari2002@gmail.com>
2024-04-14 12:30:36 +0530

    mapping and retrive

---
+++ 4.React/Keeper app part 2/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(val =>{
+        return(<Note
+          key = {val.key}
+          title = {val.title}
+          content = {val.content}
+         />);
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
