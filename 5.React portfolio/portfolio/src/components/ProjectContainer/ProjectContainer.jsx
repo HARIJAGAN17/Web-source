5d3b97a65489081997decafef03370d8b14a2787
FantasWolf <jaganhari2002@gmail.com>
2024-04-27 15:52:06 +0530

    extracted project and added projects

--- 5.React portfolio/portfolio/src/components/ProjectContainer/ProjectContainer.jsx
+++ 5.React portfolio/portfolio/src/components/ProjectContainer/ProjectContainer.jsx
@@ -1,45 +1,11 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 import './ProjectContainer.css';
-import medzone from '../../assets/medzone.jpg'
-import dodgemine from '../../assets/Dodgemine.png'
-import fitness from '../../assets/fitnesszone.jpg';
 import Project from '../Project/Project';
 import './ProjectContainer.css';
+import projects from './Project_content';
 
-const projects = [
-    {
-        image:medzone,
-        title:"Med-Zone",
-        desc:"An accessible personal healthcare web app that uses PoseNet, React, Socket.io, and TensorFlow to let users consult doctors online.",
-        link:"https://devpost.com/software/med-zone-q0uwor",
-    },
-    {
-        image:dodgemine,
-        title:"Dodge-Mine",
-        desc:"A machine learning-powered website application that uses HTML, CSS, JS, and Twilio to detect mine sweepers in the route of sailors.",
-        link:"https://devpost.com/software/dodge-mine",
-    },
-    {
-        image:fitness,
-        title:"Fitness-Zone",
-        desc:"A machine learning-powered web app that uses Google Cloud, Mediapipe, and Python to track your fitness and send you workout reminders via Twilio and Linode APIs.",
-        link:"https://devpost.com/software/fitness-zone-9um05f",
-    },
-    {
-        image:fitness,
-        title:"Fitness-Zone",
-        desc:"A machine learning-powered web app that uses Google Cloud, Mediapipe, and Python to track your fitness and send you workout reminders via Twilio and Linode APIs.",
-        link:"https://devpost.com/software/fitness-zone-9um05f",
-    },
-    {
-        image:fitness,
-        title:"Fitness-Zone",
-        desc:"A machine learning-powered web app that uses Google Cloud, Mediapipe, and Python to track your fitness and send you workout reminders via Twilio and Linode APIs.",
-        link:"https://devpost.com/software/fitness-zone-9um05f",
-    },
-   
-]
+
 
 
 function ProjectContainer() {
---
+++ 5.React portfolio/portfolio/src/components/ProjectContainer/Project_content.js
@@ -0,0 +1,41 @@
+import medzone from '../../assets/medzone.jpg'
+import dodgemine from '../../assets/Dodgemine.png'
+import fitness from '../../assets/fitnesszone.jpg';
+import dietify from "../../assets/dietify.jpg";
+import paper from "../../assets/papergram.jpg";
+
+const projects = [
+    {
+        image:medzone,
+        title:"Med-Zone",
+        desc:"An accessible personal healthcare web app that uses PoseNet, React, Socket.io, and TensorFlow to let users consult doctors online.",
+        link:"https://devpost.com/software/med-zone-q0uwor",
+    },
+    {
+        image:dodgemine,
+        title:"Dodge-Mine",
+        desc:"A machine learning-powered website application that uses HTML, CSS, JS, and Twilio to detect mine sweepers in the route of sailors.",
+        link:"https://devpost.com/software/dodge-mine",
+    },
+    {
+        image:fitness,
+        title:"Fitness-Zone",
+        desc:"A machine learning-powered web app that uses Google Cloud, Mediapipe, and Python to track your fitness and send you workout reminders via Twilio and Linode APIs.",
+        link:"https://devpost.com/software/fitness-zone-9um05f",
+    },
+    {
+        image:paper,
+        title:"PaperGram",
+        desc:"A web-based blockchain application (Web 3.0) that uses React, web3.js, Metamask, IPFS, and Polygon to safeguard user publications in the research field like IEEE.",
+        link:"https://devpost.com/software/fitness-zone-9um05f",
+    },
+    {
+        image:dietify,
+        title:"Dietify",
+        desc:"A web-based AI-powered diet planning app that uses Flutter and OpenAI API to provide personalized diet plans based on user data.",
+        link:"https://devpost.com/software/fitness-zone-9um05f",
+    },
+   
+]
+
+export default projects;
