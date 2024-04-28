5123fea4d44ceb49e1db32ad9bc07a762cbaf3bb
FantasWolf <jaganhari2002@gmail.com>
2024-04-28 09:25:40 +0530

    smoothness increased

--- 5.React portfolio/portfolio/src/components/header/header.js
+++ 5.React portfolio/portfolio/src/components/header/header.js
@@ -9,19 +9,19 @@ function Header() {
             <h1>Develop<span>er</span></h1>
         </div>
         <div className='header_right'>
-            <Link to='about' smooth='true' duration={500}>
+            <Link to='about' smooth='true' duration={700}>
             <h4>About</h4>
             </Link>
-            <Link to='skills' smooth='true' duration={500}>
+            <Link to='skills' smooth='true' duration={700}>
             <h4>Skills</h4>
             </Link>
-            <Link to='projects' smooth='true' duration={500}>
+            <Link to='projects' smooth='true' duration={700}>
             <h4>Projects</h4>
             </Link>
-            <Link to='ach' smooth='true' duration={500}>
+            <Link to='ach' smooth='true' duration={700}>
             <h4>Acheivements</h4>
             </Link>
-            <Link to='contact' smooth='true' duration={500}>
+            <Link to='contact' smooth='true' duration={700}>
             <h4>Contact</h4>
             </Link>
             <h4 className='header_right_button'>Join me</h4>
