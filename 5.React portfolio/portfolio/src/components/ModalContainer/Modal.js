2a46e24c213405e2c2292f84ec4485333ca1f76f
FantasWolf <jaganhari2002@gmail.com>
2024-04-29 20:45:58 +0530

    modal email js added

--- 5.React portfolio/portfolio/src/components/ModalContainer/Modal.js
+++ 5.React portfolio/portfolio/src/components/ModalContainer/Modal.js
@@ -3,10 +3,33 @@ import "./Modal.css";
 import { useState } from 'react';
 import close from "../../assets/close.png";
 import send from "../../assets/send.png";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 function Modal(props) {
 
   const [content, setContent] = useState({ name: "", email: "", message: "" });
+  
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs
+      .sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, {
+        publicKey: process.env.REACT_APP_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+      setContent({name:"",email:"",message:""});
+  };
+
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -18,16 +41,16 @@ function Modal(props) {
     });
   }
 
-  function checkWork(event) {
-    event.preventDefault(); // Prevent form submission
-    console.log(content);
-    setContent({ name: "", email: "", message: "" });
-  }
+  // function checkWork(event) {
+  //   event.preventDefault(); // Prevent form submission
+  //   console.log(content);
+  //   setContent({ name: "", email: "", message: "" });
+  // }
 
   return (
     <div className='modalOverlay' onClick={() => { props.onOverlay() }}>
       <div className='modalContent' onClick={(event) => { event.stopPropagation(); }}>
-        <form onSubmit={checkWork}> {/* Use onSubmit event */}
+        <form onSubmit={sendEmail} ref={form} > {/* Use onSubmit event */}
           <div className='inputBox'>
           <h2>Name <span>*</span></h2>
           <input onChange={handleChange} name="name" placeholder='Name' value={content.name} />
