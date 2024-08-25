81cbf0f8e2b13d9518ab7188760584660451d62a
Harijagan <jaganhari2002@gmail.com>
2024-08-25 10:09:32 +0530

    added typescript test files

---
+++ 6.Typescrip_practice/First.js
@@ -0,0 +1,16 @@
+var id = 2;
+var company = "HJ";
+var isPassed = true;
+var ids = [1, 2, 33, 343];
+var x = "perro";
+var xArr = ["ep", 0, true];
+// console.log(ids);
+var colors;
+(function (colors) {
+    colors[colors["red"] = 100] = "red";
+    colors[colors["blue"] = 101] = "blue";
+    colors[colors["green"] = 102] = "green";
+})(colors || (colors = {}));
+;
+var background = colors.blue;
+console.log(background);
---
+++ 6.Typescrip_practice/First.ts
@@ -0,0 +1,22 @@
+let id: number = 2;
+let company: string = "HJ";
+let isPassed: boolean  = true;
+
+
+let ids:number[] = [1,2,33,343];
+let x:any = "perro";
+
+let xArr:any[] = ["ep",0,true];
+ 
+
+// console.log(ids);
+
+
+enum colors { red=100,blue,green};
+
+let background = colors.blue;
+
+console.log(background);
+
+
+//type assertions
