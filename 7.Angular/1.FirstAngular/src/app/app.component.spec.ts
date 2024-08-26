07dcddbc7cf92d0223a355a2d42fa325b8630468
Harijagan <jaganhari2002@gmail.com>
2024-08-26 05:45:50 +0530

    remove angular version and updated to 16

--- 7.Angular/1.FirstAngular/src/app/app.component.spec.ts
+++ 7.Angular/1.FirstAngular/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '1.FirstAngular' title`, () => {
+  it(`should have as title '1.FirstAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('1.FirstAngular');
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 1.FirstAngular');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('1.FirstAngular app is running!');
   });
 });
